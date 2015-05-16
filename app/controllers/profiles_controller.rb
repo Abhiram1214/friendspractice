7a7f2237d018cda86c21d719e1f372172743e12f
Abhiram Challapalli <abhiram_cvns@hotmail.com>
2015-05-16 17:15:00 +0530

    Unknown

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,19 +3,18 @@ class ProfilesController < ApplicationController
     if current_user
       @friends = current_user.friendships
       @posts = current_user.posts
-    end  
-        
+    end
+
         puts "$$$$$$$$"
         puts @friends
         puts "$$$$$$$$"
   end
-  
+
   def user_list
     @user_all = User.all
-    
   end
-  
+
   def friends_posts
-    @friends_post = Friendship.all    
+    @friends_post = Friendship.all
   end
 end
